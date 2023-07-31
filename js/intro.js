8fb57946bde32132ac6fa10c46a126f1d43db8c0
Goran Spasojevic <goky91@gmail.com>
2023-07-31 11:03:09 +0200

    Clean up old code.

--- js/intro.js
+++
@@ -1,8 +0,0 @@
-var username = document.getElementById('input-name');
-
-var playBtn = document.getElementById('playButton');
-
-playBtn.addEventListener('click', function(){
-    localStorage.setItem('user', username.value);
-    location.href = 'game.html'
-});
